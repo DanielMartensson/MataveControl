4ebef826bd38fd00f799de6237c84629bd538293
Daniel Mårtensson <daniel.martensson100@outlook.com>
2018-02-17 12:41:10 +0100

    Add files via upload

---
+++ sourcecode/updatematavecontrol.m
@@ -0,0 +1,33 @@
+function [retval] = updatematavecontrol(varargin)
+  % Just update matavecontrol
+  A = what('matavecontrol'); % Importat that it must stand 'matavecontrol'. Nothing else!
+  
+  % Get the information about where the files are
+  path = A.path; % File path
+  
+  % Go to path dictionary
+  cd(A.path);
+  
+  % Downloading listOfFunctions
+  urlwrite('https://raw.githubusercontent.com/DanielMartensson/matavecontrol/master/listOfFunctions', 'listOfFunctions');
+  
+  % Read the listOfFunctions
+  fid = fopen('listOfFunctions');
+  txt = textscan(fid, '%s', 'delimiter', ','); % Importat to have the delimiter = ','
+  % Remove this double struct
+  fileList = txt{1,1};
+  
+  % Downloading all the other files
+  [m, n] = size(fileList);
+  for i = 1:m
+    % Downloading
+    nameOfFile = fileList{i, 1};
+    URL = strcat('https://raw.githubusercontent.com/DanielMartensson/matavecontrol/master/sourcecode/', nameOfFile)
+    [saveplace, Success] = urlwrite(URL, nameOfFile);
+    saveplace
+    isSuccess
+  end
+  
+  disp('Matavecontrol is updated!')
+  
+end
