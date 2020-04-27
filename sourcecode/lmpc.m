e97b25ff7ece84e5e258d520ab6d5169f8e93680
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-04-28 01:37:54 +0200

    Model Predictive Control - Linear Programming for MATLAB now

--- sourcecode/lmpc.m
+++ sourcecode/lmpc.m
@@ -1,13 +1,13 @@
-% Use linear Model Predictive Control with limits on inputs
-% Input: sysd, N(horizon), r(Reference vector), T(End time), u_lb(Min limit input), u_ub(Max limit input), y_lb(Min limit output), y_ub(Max limit output), x0(Initial state)
-% Example 1: [y, t, x, u] = lmpc(sysd, N, r, T, u_lb, u_ub, y_lb, y_ub)
-% Example 2: [y, t, x, u] = lmpc(sysd, N, r, T, u_lb, u_ub, y_lb, y_ub, x0)
+% Use linear Model Predictive Control
+% Input: sysd(Discrete state space model), N(Horizon), r(Reference vector), t(Time vector), regularization(optimal), x0(Initial state, optimal)
+% Output: y(Output signal), T(Discrete time vector), X(State vector), U(Output signal)
+% Example 1: [y, T, X, U] = lmpc(sysd, N, r, t)
+% Example 2: [y, T, X, U] = lmpc(sysd, N, r, t, regularization)
+% Example 3: [y, T, X, U] = lmpc(sysd, N, r, t, regularization, x0)
 % Author: Daniel Mårtensson
-% Update: Added Linear programming too. 2020-04-10
+% Update: Replaced QP solver with LP solver due to CControl library - Sorry! 
 
-% Can only be still used with Octave due to the QP-command! I will work on that!
-
-function [y, t, X, U] = lmpc(varargin)
+function [y, T, X, U] = lmpc(varargin)
   % Check if there is any input
   if(isempty(varargin))
     error ('Missing model')
@@ -48,76 +48,37 @@ function [y, t, X, U] = lmpc(varargin)
     
     % Get the total time 
     if(length(varargin) >= 4)
-      T = varargin{4};
-      t = 0:sampleTime:T; % Get time vector - Also return it!
+      t = varargin{4};
     else
-      error('Missing the total time number T');
+      error('Missing the time vector');
     end
     
-    % Get the lower bound vector u_lb 
+    % Get the regularization parameter
     if(length(varargin) >= 5)
-      u_lb = varargin{5};
+      regularization = varargin{5};
     else
-      error('Need lower bound vector u_lb')
-    end
-    
-    % Get the upper bound vector u_ub 
-    if(length(varargin) >= 6)
-      u_ub = varargin{6};
-    else
-      error('Need lower bound vector u_ub')
-    end
-    
-    % Get the minimum input signal limit
-    if(length(varargin) >= 7)
-      y_lb = varargin{7};
-    else
-      error('Need minimum input signal limit')
-    end
-    
-    % Get the maximum input signal limit
-    if(length(varargin) >= 8)
-      y_ub = varargin{8};
-    else
-      error('Need maximum input signal limit')
+      regularization = 0;
     end
     
     % Get the initial trajectory vector x
-    if(length(varargin) >= 9)
-      x = varargin{9};
+    if(length(varargin) >= 6)
+      x = varargin{6};
     else
       x = zeros(size(A, 1), 1);
     end
     
-    % Compute the PHI matrix now!
+    %% Solve this formula: R = PHI*x + GAMMA*U, where we want U
+    
+    % Compute the PHI and GAMMA matrix now
     PHI = phiMat(A, C, N);
-    % Compute the GAMMA matrix now
     GAMMA = gammaMat(A, B, C, N);
-    % Compute the tuning matrix - Set it to identity matrix
-    alpha = 0.001; % Regularization parameter
-    Q = alpha*eye(size(GAMMA, 1), size(GAMMA, 1)); 
-    % Compute H matrix
-    H = GAMMA'*Q*GAMMA;
-    % Create initial input signal 
-    q = GAMMA'*Q*PHI*x - GAMMA'*Q*R;
-    % Choose the lower bounds and upper bounds limits for input
-    U_LB = repmat(u_lb, N, 1);
-    U_UB = repmat(u_ub, N, 1);
-    % Choose the lower bounds and upper bounds limits for output
-    A_IN = GAMMA;
-    Y_LB = repmat(y_lb, N, 1) - PHI*x;
-    Y_UB = repmat(y_ub, N, 1) - PHI*x;
-    % Compute the first input signals!
-    u = qp([], H, q, [], [], U_LB, U_UB, Y_LB, A_IN, Y_UB);
-   
-    % This is for LINEAR PROGRAMMING - uncomment them all
-    % This is on the form max: c^Tx, St: Ax <= b, x >= 0.
-    % Notice that this have Tikhonov Regularization included
-    %b = R - PHI*x;
-    %CTYPE = repmat(["U"], 1, size(A_IN, 1));
-    %VARTYPE = repmat(["C"], 1, size(A_IN, 2));
-    %u = glpk((GAMMA'*GAMMA + alpha*eye(size(GAMMA)))'*b, (GAMMA'*GAMMA + alpha*eye(size(GAMMA))), GAMMA'*b, U_LB, [], CTYPE, VARTYPE, -1);
-
+    
+    % We using Tikhonov regularization when we are using LP: Max c^T, S.t: Ax <= b, x >= 0
+    % clp = (A'*A + lambda*I)'*b
+    % blp = A'*b
+    % alp = A'*A + lambda*I
+    alp = GAMMA'*GAMMA + regularization*eye(size(GAMMA'*GAMMA));
+    u = zeros(size(alp, 2), size(B, 2)); % Prevent over shoot from the beginning
     
     % Find the optimal input signals U from the QP-formula: J = 0.5*U'H*U + U'*q
     for k = 1:size(t,2) 
@@ -128,14 +89,13 @@ function [y, t, X, U] = lmpc(varargin)
       % Compute outputs
       y(:,k) = C*x + D*u(1:size(B, 2)); % size(B, 2) = If multiple inputs...
       
-      % Update states and input signals and q matrix
+      % Update states
       x = A*x + B*u(1:size(B, 2)); 
-      q = GAMMA'*Q*PHI*x - GAMMA'*Q*R;
-      u = qp([], H, q, [], [], U_LB, U_UB, Y_LB, A_IN, Y_UB);
       
-      % This is for LINEAR PROGRAMMING - uncomment them all
-      %b = R - PHI*x;
-      %u = glpk((GAMMA'*GAMMA + alpha*eye(size(GAMMA)))'*b, (GAMMA'*GAMMA + alpha*eye(size(GAMMA))), GAMMA'*b, U_LB, [], CTYPE, VARTYPE, -1);
+      % Update the constraints and objective function
+      clp = (GAMMA'*GAMMA + regularization*eye(size(GAMMA'*GAMMA)))'*(R-PHI*x);
+      blp = GAMMA'*(R-PHI*x);
+      u = linprog2(clp, alp, blp, 0, 200);
     end
     
     % Change t and y vector and u so the plot look like it is discrete - Important!
@@ -164,7 +124,7 @@ function [y, t, X, U] = lmpc(varargin)
     end
         
     % Just remove the first one 
-    t = t(:,2:length(t));
+    T = t(:,2:length(t));
     % And the last one
     y = y(:,1:(length(y)-1));
     % And for U and X too
@@ -175,7 +135,7 @@ function [y, t, X, U] = lmpc(varargin)
     % Plot - How many subplots?
     for i = 1:size(C,1)
       subplot(size(C,1),1,i)
-      plot(t, y(i,:)); 
+      plot(T, y(i,:)); 
       ylabel(strcat('y', num2str(i)));
       if (sampleTime > 0)
         xlabel(strcat(num2str(sampleTime), ' time unit/sample'));
@@ -185,8 +145,145 @@ function [y, t, X, U] = lmpc(varargin)
       grid on
     end
     
-  endif
-endfunction
+  end
+end
+
+
+% This simplex method has been written as it was C code
+function [x] = linprog2(c, A, b, max_or_min, iteration_limit)
+  row_a = size(A, 1);
+  column_a = size(A, 2);
+  
+  if(max_or_min == 0)
+    % Maximization
+    x = opti(c, A, b, row_a, column_a, max_or_min, iteration_limit);
+  else
+    % Minimization
+    x = opti(b, A', c, column_a, row_a, max_or_min, iteration_limit);
+  end
+end
+
+
+function [x] = opti(c, A, b, row_a, column_a, max_or_min, iteration_limit)
+  
+  % Clear the solution
+	if(max_or_min == 0)
+		x = zeros(column_a, 1);
+	else
+		x = zeros(row_a, 1);
+  end
+  
+  % Create the tableau
+  tableau = zeros(row_a + 1, column_a + row_a + 2);
+  j = 1;
+  for i = 1:row_a
+    % First row
+    tableau(i, 1:column_a) = A(i, 1:column_a);
+    
+    % Slack variable s
+    j = column_a + i;
+    tableau(i, j) = 1;
+    
+    % Add b vector
+    tableau(i, column_a + row_a + 2) = b(i);
+  end
+  
+  % Negative objective function
+  tableau(row_a + 1, 1:column_a) = -c(1:column_a);
+ 
+  % Slack variable for objective function
+  tableau(row_a + 1, column_a + row_a + 1) = 1;
+  
+  % Do row operations
+	entry = -1.0; % Need to start with a negative number because MATLAB don't have do-while! ;(
+	pivotColumIndex = 0;
+	pivotRowIndex = 0;
+	pivot = 0.0;
+	value1 = 0.0;
+	value2 = 0.0;
+	value3 = 0.0;
+	smallest = 0.0;
+	count = 0;
+  while(entry < 0) % Continue if we have still negative entries
+    % Find our pivot column
+    pivotColumIndex = 1;
+    entry = 0.0;
+    for i = 1:column_a + row_a + 2
+      value1 = tableau(row_a + 1, i);
+      if(value1 < entry)
+        entry = value1;
+        pivotColumIndex = i;
+      end
+    end
+    
+    % If the smallest entry is equal to 0 or larger than 0, break
+    if(or(entry >= 0.0, count >= iteration_limit))
+      break;
+    end 
+    
+    % Find our pivot row
+    pivotRowIndex = 1;
+    value1 = tableau(1, pivotColumIndex); % Value in pivot column
+    value2 = tableau(1, column_a+row_a+2); % Value in the b vector
+    smallest = value2/value1; % Initial smalles value1
+    for i = 2:row_a
+      value1 = tableau(i, pivotColumIndex); % Value in pivot column
+      value2 = tableau(i, column_a+row_a+2); % Value in the b vector
+      value3 = value2/value1;
+      if(or(and(value3 > 0, value3 < smallest), smallest < 0))
+        smallest = value3;
+        pivotRowIndex = i;
+      end 
+    end
+    
+    % We know where our pivot is. Turn the pivot into 1
+    % 1/pivot * PIVOT_ROW -> PIVOT_ROW
+    pivot = tableau(pivotRowIndex, pivotColumIndex); % Our pivot value
+    for i = 1:column_a + row_a + 2
+      value1 = tableau(pivotRowIndex, i); % Our row value at pivot row
+      tableau(pivotRowIndex, i) = value1 * 1/pivot; % When value1 = pivot, then pivot will be 1
+    end
+    
+    % Turn all other values in pivot column into 0. Jump over pivot row
+		% -value1* PIVOT_ROW + ROW -> ROW
+    for i = 1:row_a + 1
+      if(i ~= pivotRowIndex)
+        value1 = tableau(i, pivotColumIndex); %  This is at pivot column
+        for j = 1:column_a+row_a+2
+          value2 = tableau(pivotRowIndex, j); % This is at pivot row
+          value3 = tableau(i, j); % This is at the row we want to be 0 at pivot column
+          tableau(i, j) = -value1*value2 + value3;
+        end
+      end
+    end
+    
+    % Count for the iteration
+		count = count + 1;
+  end
+  
+  % If max_or_min == 0 -> Maximization problem
+  if(max_or_min == 0)
+    % Now when we have shaped our tableau. Let's find the optimal solution. Sum the columns
+    for i = 1:column_a
+      value1 = 0; % Reset
+      for j = 1:row_a + 1
+        value1 = value1 + tableau(j, i); % Summary
+        value2 = tableau(j, i); %  If this is 1 then we are on the selected
+        
+        % Check if we have a value that are very close to 1
+        if(and(and(value1 < 1 + eps, value1 > 1 - eps), value2 > 1 - eps))
+          x(i) = tableau(j, column_a+row_a+2);
+        end
+      end
+    end
+  else
+    % Minimization (The Dual method) - Only take the bottom rows on the slack variables
+    for i = 1:row_a
+      x(i) = tableau(row_a+1, i + column_a); % We take only the bottom row at start index column_a
+    end
+  end
+    
+end
 
 
 function PHI = phiMat(A, C, N)
