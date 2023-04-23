531a8e1fdd72edfc228b1e92e9818bd123f0eb98
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-04-23 13:55:09 +0200

    Extracted out quadprog.m

--- sourcecode/qmpc.m
+++ sourcecode/qmpc.m
@@ -197,62 +197,6 @@ function [Y, T, X, U] = qmpc(varargin)
   title('Model Predictive Control With Integral Action And Quadratic Programming')
 end
 
-function [x, solution] = quadprog(Q, c, A, b)
-  % Assume that the solution is true
-  solution = true;
-
-  % Set number of iterations
-  number_of_iterations = 255;
-
-  % Same as in C code
-  FLT_EPSILON = 1.19209290e-07;
-
-  % Unconstrained solution
-  x = -linsolve(Q, c);
-
-  % Constraints difference
-  K = b - A*x;
-
-  % Check constraint violation
-  if(sum(K <= 0) == 0)
-    return; % No violation
-  end
-
-  % Create P
-  P = linsolve(Q, A');
-
-  % Create H = A*Q*A'
-  H = A*P;
-
-  % Solve lambda from H*lambda = -K, where lambda >= 0
-  [m, n] = size(K);
-  lambda = zeros(m, n);
-  for km = 1:number_of_iterations
-    lambda_p = lambda;
-
-    % Use Gauss Seidel
-    for i = 1:m
-      w = -1.0/H(i,i)*(K(i) + H(i,:)*lambda - H(i,i)*lambda(i));
-      lambda(i) = max(0, w);
-    end
-
-    % Check if the minimum convergence has been reached
-    w = (lambda - lambda_p)'*(lambda - lambda_p);
-    if (w < FLT_EPSILON)
-      break;
-    end
-
-    % Check if the maximum iteration have been reached
-    if(km == 255)
-      solution = false;
-      return;
-    end
-  end
-
-  % Find the solution: x = -inv(Q)*c - inv(Q)*A'*lambda
-  x = x - P*lambda;
-end
-
 function PHI = phiMat(A, C, N)
 
   % Create the special Observabillity matrix
